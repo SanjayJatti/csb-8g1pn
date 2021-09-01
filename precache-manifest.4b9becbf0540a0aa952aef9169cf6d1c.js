8511c4e27f624862f605f4434e748f02b8df939f
Code Sandbox <hello@codesandbox.io>
2021-09-01 06:47:42 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.4b9becbf0540a0aa952aef9169cf6d1c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d8847f06a83dd867edaae89f8b049de",
+    "url": "https://sanjayjatti.github.io/csb-8g1pn/index.html"
+  },
+  {
+    "revision": "1233c45e7312f66a376b",
+    "url": "https://sanjayjatti.github.io/csb-8g1pn/static/css/main.d00611c3.chunk.css"
+  },
+  {
+    "revision": "b1da79603a7f80f77924",
+    "url": "https://sanjayjatti.github.io/csb-8g1pn/static/js/2.3dc7f149.chunk.js"
+  },
+  {
+    "revision": "464fef3691779b3926634327df24dae8",
+    "url": "https://sanjayjatti.github.io/csb-8g1pn/static/js/2.3dc7f149.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1233c45e7312f66a376b",
+    "url": "https://sanjayjatti.github.io/csb-8g1pn/static/js/main.0c51812e.chunk.js"
+  },
+  {
+    "revision": "945f92fb4bdec879b132",
+    "url": "https://sanjayjatti.github.io/csb-8g1pn/static/js/runtime-main.05d9d631.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "https://sanjayjatti.github.io/csb-8g1pn/precache-manifest.4b9becbf0540a0aa952aef9169cf6d1c.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://sanjayjatti.github.io/csb-8g1pn/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
